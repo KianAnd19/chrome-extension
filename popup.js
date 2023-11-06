4f461eb8e6c84d2b35e012b3ba503420f9a89e6c
Kian Anderson <24872849@sun.ac.za>
2023-11-07 00:13:02 +0200

    Change start button to be a toggle button

--- popup.js
+++ popup.js
@@ -1,36 +1,48 @@
-let isRunning = false;
-let pomodoroTime = 25 * 60; // 25 minutes
-let countdown;
-
-document.getElementById('start-btn').addEventListener('click', () => {
-  if (!isRunning) {
-    isRunning = true;
-    countdown = setInterval(() => {
-      pomodoroTime--;
-      displayTime(pomodoroTime);
-      if (pomodoroTime <= 0) {
+// Ensure the DOM content is loaded before trying to access the elements
+document.addEventListener('DOMContentLoaded', function() {
+    let isRunning = false;
+    let pomodoroTime = 25 * 60; // 25 minutes
+    let countdown;
+  
+    const toggleBtn = document.getElementById('toggle-btn');
+    const resetBtn = document.getElementById('reset-btn');
+    // Remove this if you no longer have a 'pause-btn'
+    // const pauseBtn = document.getElementById('pause-btn');
+  
+    toggleBtn.addEventListener('click', function() {
+      if (isRunning) {
+        this.textContent = 'Start';
         clearInterval(countdown);
-        // Here you can also trigger a Chrome notification
+        isRunning = false;
+      } else {
+        this.textContent = 'Pause';
+        isRunning = true;
+        countdown = setInterval(function() {
+          pomodoroTime--;
+          displayTime(pomodoroTime);
+          if (pomodoroTime <= 0) {
+            clearInterval(countdown);
+            isRunning = false;
+            this.textContent = 'Start';
+            // Here you can also trigger a Chrome notification
+          }
+        }, 1000);
       }
-    }, 1000);
-  }
-});
-
-document.getElementById('pause-btn').addEventListener('click', () => {
-  clearInterval(countdown);
-  isRunning = false;
-});
-
-document.getElementById('reset-btn').addEventListener('click', () => {
-  clearInterval(countdown);
-  pomodoroTime = 25 * 60;
-  displayTime(pomodoroTime);
-  isRunning = false;
-});
-
-function displayTime(seconds) {
-  let minutes = Math.floor(seconds / 60);
-  let remSeconds = seconds % 60;
-  document.getElementById('timer-display').textContent = 
-    `${minutes}:${remSeconds < 10 ? '0' : ''}${remSeconds}`;
-}
+    });
+  
+    resetBtn.addEventListener('click', function() {
+      clearInterval(countdown);
+      pomodoroTime = 25 * 60;
+      displayTime(pomodoroTime);
+      toggleBtn.textContent = 'Start';
+      isRunning = false;
+    });
+  
+    function displayTime(seconds) {
+      let minutes = Math.floor(seconds / 60);
+      let remSeconds = seconds % 60;
+      document.getElementById('timer-display').textContent =
+        `${minutes}:${remSeconds < 10 ? '0' : ''}${remSeconds}`;
+    }
+  });
+  
